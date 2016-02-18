6f7d57df16c51a0acc467a9f2f56b192b204e661
Daniel <truenorthtrainingcompany@gmail.com>
2016-02-18 16:14:49 -0500

    Haikus marked as read Will have background colour changed to dark grey Later on this will remove haiku from main list and add to a new list of recently read haikus

--- client/components/haiku.jsx
+++ client/components/haiku.jsx
@@ -4,13 +4,30 @@ Haiku = React.createClass({
     // component displays haiku through react prop
     haiku: React.PropTypes.object.isRequired
   },
+
+  toggleChecked() {
+    // set checked property to opposite of its current value
+    Haikus.update(this.props.haiku._id, {
+      $set: {checked: ! this.props.haiku.checked}
+    });
+  },
+
   render() {
+    // toggle a className so that CSS can hide or display haikus
+    const haikuClassName = this.props.haiku.checked ? "checked" : "";
+
     return (
-      <div>
+      <div className={haikuClassName}>
         <h3>{this.props.haiku.title}</h3>
         <li>{this.props.haiku.line1}</li>
         <li>{this.props.haiku.line2}</li>
         <li>{this.props.haiku.line3}</li>
+        <li>Mark as read
+          <input type="checkbox"
+            readOnly={true}
+            checked={this.props.haiku.checked}
+            onClick={this.toggleChecked} />
+        </li>
       </div>
     );
   }
